0137a9b743b9074ccf369111e908475e59741898
Georgy Levchenko <levchenkoga@pik.ru>
2021-11-30 16:46:12 +0400

    Added unversioned files.

---
+++ src/UI/DesktopClearArchitecture.Client/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DesktopClearArchitecture.Client
+{
+    using System.Windows;
+    using Prism.Ioc;
+    using Views;
+
+    /// <inheritdoc />
+    public partial class App
+    {
+        /// <inheritdoc />
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override Window CreateShell() => Container.Resolve<MainWindow>();
+    }
+}
---
+++ src/UI/DesktopClearArchitecture.Client/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DesktopClearArchitecture.Client.Views
+{
+    /// <inheritdoc cref="System.Windows.Window" />
+    public partial class MainWindow
+    {
+        /// <inheritdoc />
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
