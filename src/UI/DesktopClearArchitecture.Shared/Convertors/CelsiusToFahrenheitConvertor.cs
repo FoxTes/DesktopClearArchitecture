214a1f7ebfd2bd923038707a2a1528b85c2beed9
Georgy Levchenko <lewshadow@gmail.com>
2022-02-14 10:36:09 +0400

    Added an example of a converter.

---
+++ src/UI/DesktopClearArchitecture.Shared/Convertors/CelsiusToFahrenheitConvertor.cs
@@ -0,0 +1,28 @@
+﻿namespace DesktopClearArchitecture.Shared.Convertors;
+
+using UnitsNet;
+using System.Globalization;
+using System.Windows.Data;
+
+/// <summary>
+/// Converts degrees Celsius to Fahrenheit.
+/// </summary>
+[ValueConversion(typeof(double), typeof(string))]
+public class CelsiusToFahrenheitConvertor : IValueConverter
+{
+    /// <inheritdoc />
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double temp)
+            return Math.Round(Temperature.FromDegreesCelsius(temp).DegreesFahrenheit);
+        throw new Exception("Value is not double type.");
+    }
+
+    /// <inheritdoc />
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double temp)
+            return Math.Round(Temperature.FromDegreesFahrenheit(temp).DegreesCelsius);
+        throw new Exception("Value is not double type.");
+    }
+}
---
+++ tests/DesktopClearArchitecture.UI.Shared/ConvertorsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace DesktopClearArchitecture.UI.Shared;
+
+using System.Globalization;
+using DesktopClearArchitecture.Shared.Convertors;
+using FluentAssertions;
+using Xunit;
+
+/// <summary>
+/// Convertors test.
+/// </summary>
+public class ConvertorsTests
+{
+    /// <summary>
+    /// Celsius to fahrenheit test.
+    /// </summary>
+    /// <param name="celsius">Celsius.</param>
+    /// <param name="fahrenheit">Fahrenheit.</param>
+    [Theory]
+    [InlineData(0d, 32d)]
+    [InlineData(1d, 34d)]
+    [InlineData(-101d, -150d)]
+    public void CelsiusToFahrenheitConvertTest(double celsius, double fahrenheit)
+    {
+        var convertor = new CelsiusToFahrenheitConvertor();
+
+        convertor.Convert(celsius, typeof(double), null, new CultureInfo("en-us"))
+            .Should()
+            .Be(fahrenheit);
+    }
+
+    /// <summary>
+    /// Fahrenheit ti celsius test.
+    /// </summary>
+    [Fact]
+    public void FahrenheitToCelsiusConvertTest()
+    {
+        var convertor = new CelsiusToFahrenheitConvertor();
+
+        convertor.ConvertBack(32d, typeof(double), null, new CultureInfo("en-us"))
+            .Should()
+            .Be(0d);
+    }
+}
